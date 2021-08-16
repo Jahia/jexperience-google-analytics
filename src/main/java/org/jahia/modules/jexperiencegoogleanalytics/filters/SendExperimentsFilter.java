08b24f755e022d069b27f201ecae80693b5663b7
kevan Jahanshahi <kevan@jahia.com>
2021-08-16 17:05:44 +0200

    TECH-355: remove experiments GA code, and cleanup (#6) (#9)
    * TECH-355: remove experiments GA code, and cleanup (#6)
    
    * TECH-355: remove experiments GA code, and cleanup
    
    * TECH-355: remvoe contentRetrieval dependency
    
    * TECH-355: cleanup dependencies and pom.xml

--- src/main/java/org/jahia/modules/jexperiencegoogleanalytics/filters/SendExperimentsFilter.java
+++ src/main/java/org/jahia/modules/jexperiencegoogleanalytics/filters/SendExperimentsFilter.java
@@ -74,19 +74,14 @@ import java.io.Writer;
 import java.util.List;
 
 /**
+ * Filter for injecting google jexperience js into the page header
  * Created by rahmed on 03/08/17.
  */
-public class SendExperimentsFilter extends AbstractFilter implements ApplicationListener<ApplicationEvent>, JahiaModuleAware {
+public class SendExperimentsFilter extends AbstractFilter implements JahiaModuleAware {
 
     private static Logger logger = LoggerFactory.getLogger(SendExperimentsFilter.class);
 
-    private ScriptEngineUtils scriptEngineUtils;
-
-    private String template;
-
     private JahiaTemplatesPackage module;
-
-    private String resolvedTemplate;
 
     public SendExperimentsFilter() {
         //Execute filter only if module is enabled on the site
@@ -106,94 +101,22 @@ public class SendExperimentsFilter extends AbstractFilter implements Application
 
     @Override
     public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
-        String out = previousOut;
-        JCRSessionWrapper session = resource.getNode().getSession();
-        String script = getResolvedTemplate();
-        if (script != null) {
-            String scriptUrl = renderContext.getURLGenerator().getContext() + "/modules/jexperience-google-analytics/javascript/google_experiments.js";
-            Source source = new Source(previousOut);
-            OutputDocument outputDocument = new OutputDocument(source);
-            List<Element> headElementList = source.getAllElements(HTMLElementName.HEAD);
-            for (Element element : headElementList) {
-                final EndTag headEndTag = element.getEndTag();
-                String extension = StringUtils.substringAfterLast(template, ".");
-                ScriptEngine scriptEngine = scriptEngineUtils.scriptEngine(extension);
-                ScriptContext scriptContext = new GoogleScriptContext();
-                final Bindings bindings = scriptEngine.createBindings();
-                bindings.put("scriptUrl", scriptUrl);
-                bindings.put("jcrContext", renderContext.getURLGenerator().getContext());
-                bindings.put("jcrWorkspace", Constants.LIVE_WORKSPACE);
-                bindings.put("jcrLocale", session.getLocale());
-                String url = resource.getNode().getUrl();
-                if (renderContext.getRequest().getAttribute("analytics-path") != null) {
-                    url = (String) renderContext.getRequest().getAttribute("analytics-path");
-                }
-                bindings.put("resourceUrl", url);
-                bindings.put("resource", resource);
-                scriptContext.setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
-                // The following binding is necessary for Javascript, which doesn't offer a console by default.
-                bindings.put("out", new PrintWriter(scriptContext.getWriter()));
+        String scriptUrl = renderContext.getURLGenerator().getContext() + "/modules/jexperience-google-analytics/javascript/google_experiments.js";
+        Source source = new Source(previousOut);
+        OutputDocument outputDocument = new OutputDocument(source);
+        List<Element> headElementList = source.getAllElements(HTMLElementName.HEAD);
 
-                scriptEngine.eval(script, scriptContext);
-                StringWriter writer = (StringWriter) scriptContext.getWriter();
-
-                final String googleScript = writer.toString();
-                if (StringUtils.isNotBlank(googleScript)) {
-                    outputDocument.replace(headEndTag.getBegin(), headEndTag.getBegin() + 1,
-                            "\n" + AggregateCacheFilter.removeEsiTags(googleScript) + "\n<");
-                }
-                break; // avoid to loop if for any reasons multiple body in the page
-            }
-            out = outputDocument.toString().trim();
-        }
-        return out;
-    }
-
-    protected String getResolvedTemplate() throws IOException {
-        if (resolvedTemplate == null) {
-            resolvedTemplate = WebUtils.getResourceAsString(template);
-            if (resolvedTemplate == null) {
-                logger.warn("Unable to lookup template at {}", template);
-            }
+        for (Element element : headElementList) {
+            final EndTag headEndTag = element.getEndTag();
+            outputDocument.replace(headEndTag.getBegin(), headEndTag.getBegin() + 1, "\n<script src=\"" + scriptUrl + "\"></script>\n<");
+            break; // avoid to loop if for any reasons multiple body in the page
         }
 
-        return resolvedTemplate;
-    }
-
-    public void onApplicationEvent(ApplicationEvent event) {
-        JahiaTemplateManagerService service;
-        if (event instanceof JahiaTemplateManagerService.TemplatePackageRedeployedEvent) {
-            resolvedTemplate = null;
-        }
-    }
-
-    public void setScriptEngineUtils(ScriptEngineUtils scriptEngineUtils) {
-        this.scriptEngineUtils = scriptEngineUtils;
-    }
-
-    public void setTemplate(String template) {
-        this.template = template;
+        return outputDocument.toString().trim();
     }
 
     @Override
     public void setJahiaModule(JahiaTemplatesPackage jahiaTemplatesPackage) {
-
         this.module = jahiaTemplatesPackage;
-
-    }
-
-    protected static class GoogleScriptContext extends SimpleScriptContext {
-        private Writer writer = null;
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public Writer getWriter() {
-            if (writer == null) {
-                writer = new StringWriter();
-            }
-            return writer;
-        }
     }
 }
--- src/main/resources/WEB-INF/scripts/send_experiments.groovy
+++
@@ -1,6 +0,0 @@
-println("<script type=\"text/javascript\">" +
-        "jcrContext = '${jcrContext}';" +
-        "jcrWorkspace = '${jcrWorkspace}';" +
-        "jcrLocale = '${jcrLocale}';" +
-        "</script>")
-println("<script src=\"${scriptUrl}\"></script>")
